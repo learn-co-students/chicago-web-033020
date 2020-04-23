c1990b2a52ab30128fb4b18959eb3bcc78b27760
dukegreene <dukegreene@gmail.com>
2020-04-23 10:46:51 -0500

    add cul de cats

--- cul-de-cats/app/controllers/cats_controller.rb
+++ cul-de-cats/app/controllers/cats_controller.rb
@@ -1,25 +1,30 @@
 class CatsController < ApplicationController
 
   # Index
-  get '/cats' do
+  get '/cats/?' do
     @cats = Cat.all
     erb :'cats/index'
   end
 
   # New
-  get '/cats/new' do
+  get '/cats/new/?' do
+    @cat = Cat.new
     erb :'cats/new'
   end
 
   # Create
-  post '/cats' do
-    cat = Cat.create(name: params[:name], claw_count: params[:claw_count], coarseness: params[:coarseness])
-    p cat
-    redirect "/cats/#{cat.id}"
+  post '/cats?' do
+    @cat = Cat.new(name: params[:name], claw_count: params[:claw_count], coarseness: params[:coarseness])
+    if @cat.valid?
+      @cat.save
+      redirect "/cats/#{@cat.id}"
+    else
+      erb :'cats/new'
+    end
   end
   
   # Show
-  get '/cats/:id' do
+  get '/cats/:id/?' do
     id_to_search = params[:id]
     @cat = Cat.find(id_to_search)
     erb :'cats/show'
--- cul-de-cats/app/models/cat.rb
+++ cul-de-cats/app/models/cat.rb
@@ -1,8 +1,22 @@
 class Cat < ActiveRecord::Base
   has_many :yards
 
+  validates_presence_of :name, :claw_count, :coarseness
+  validates_uniqueness_of :name, case_sensitive: false
+  validates_numericality_of :claw_count, greater_than: 0
+  validates_format_of :name, { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters, do not let the humans and their IOT tech win" }
+  validates_uniqueness_of :claw_count, scope: :coarseness
+  validate :no_seven_clawed_a_named_cats
+
   def self.meow_is_real(sound)
     # Return true if the sound contains a meow
     !!sound.match(/me.*ow/i)
+  end
+
+  def no_seven_clawed_a_named_cats
+    if name[0].downcase == "a" && claw_count == 7
+      self.errors.add(:name, "cannot start with letter 'A' so long as there are 7 claws on ye")
+    end
   end
 end
