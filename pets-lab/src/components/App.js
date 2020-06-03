d44b92b6b50b6a30b689fcd086bcda3955059e66
Raza Jafri <sjafri@SF-US04304-MBP13.local>
2020-06-03 17:39:33 -0500

    fo

---
+++ pets-lab/src/components/App.js
@@ -0,0 +1,81 @@
+import React from 'react'
+
+import Filters from './Filters'
+import PetBrowser from './PetBrowser'
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      pets: [],
+      filters: {
+        type: 'all'
+      }
+    }
+  }
+
+  onChangeType = (event) => {
+    this.setState({
+      filters: {
+        type: event.target.value
+      }
+    })
+
+  }
+
+  onFindPetsClick= () => {
+    let url = '/api/pets'
+    if (this.state.filters.type !== 'all')  {
+      url = `/api/pets?type=${this.state.filters.type}`
+    } 
+
+
+    fetch(url)
+    .then(resp => resp.json())
+    .then(jsonData => {
+      this.setState({
+        pets: jsonData
+      })
+    })
+  }
+
+  onAdoptPet = (adoptedPetId) => {
+    const updatedPets = this.state.pets.map(pet => {
+      if (pet.id === adoptedPetId) {
+        return {
+          ...pet,
+          isAdopted: true
+        }
+      } else {
+        return pet
+      }
+    })
+    this.setState({
+      pets: updatedPets
+    })
+  }
+
+  render() {
+    console.log(this.state.pets)
+    return (
+      <div className="ui container">
+        <header>
+          <h1 className="ui dividing header">React Animal Shelter</h1>
+        </header>
+        <div className="ui container">
+          <div className="ui grid">
+            <div className="four wide column">
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
+            </div>
+            <div className="twelve wide column">
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
